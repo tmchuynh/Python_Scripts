e01be06834323bed10479ba5e623096da43186fe
tmchuynh <tinamchuynh@gmail.com>
2023-06-18 14:49:33 -0700

    add validations

--- password_generator.py
+++ password_generator.py
@@ -15,14 +15,26 @@ def generate_password(length=12, use_punctuation=True, use_numbers=True):
     return password
 
 # Prompt the user for the desired number of passwords
-num_passwords = int(input("Enter the number of passwords you want: "))
+while True:
+    num_passwords_input = input("Enter the number of passwords you want: ")
+    if num_passwords_input.isdigit():
+        num_passwords = int(num_passwords_input)
+        break
+    else:
+        print("Invalid input. Please enter a valid number.")
 
 # Prompt the user for the desired password length
-password_length = int(input("Enter the desired password length: "))
+while True:
+    password_length_input = input("Enter the desired password length: ")
+    if password_length_input.isdigit():
+        password_length = int(password_length_input)
+        break
+    else:
+        print("Invalid input. Please enter a valid number.")
 
 # Prompt the user for whether to include punctuation or numbers
-use_numbers = input("Do you want to include numbers? (y/n): ").lower() == 'y'
 use_punctuation = input("Do you want to include punctuation? (y/n): ").lower() == 'y'
+use_numbers = input("Do you want to include numbers? (y/n): ").lower() == 'y'
 
 # Generate and print the passwords
 passwords = [generate_password(password_length, use_punctuation, use_numbers) for _ in range(num_passwords)]
